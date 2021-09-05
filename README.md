a40b8e20cc277e90f266b35e87804eabd1fa459c
yodasoda1219 <theo.github@bedafamily.com>
2021-09-05 15:58:05 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# install-fnalibs
+
+A simple Python 3 script to install native [FNA](https://fna-xna.github.io) dependencies.
+
+Dependencies:
+- [cmake](https://cmake.org)
+- [git](https://git-scm.com)
---
+++ install_fnalibs.py
@@ -0,0 +1,112 @@
+"""
+   Copyright 2021 Lucy Beda
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+import subprocess
+import os
+import os.path as path
+import argparse
+import shutil
+
+class LibraryDescriptor:
+    def __init__(self, name: str, remote: str):
+        self.name = name
+        self.remote = remote
+    def clone(self, fnalibs_dir: str):
+        repo_path = path.join(fnalibs_dir, self.name)
+        if path.isdir(repo_path):
+            shutil.rmtree(repo_path)
+        elif path.exists(repo_path):
+            os.unlink(repo_path)
+        args = [
+            "git",
+            "clone",
+            "--recursive",
+            self.remote,
+            repo_path
+        ]
+        return subprocess.call(args) == 0
+LIBS = [
+    LibraryDescriptor("SDL2", "https://github.com/libsdl-org/SDL.git"),
+    LibraryDescriptor("FNA3D", "https://github.com/FNA-XNA/FNA3D.git"),
+    LibraryDescriptor("FAudio", "https://github.com/FNA-XNA/FAudio.git")
+]
+SCRIPT_DIR = path.dirname(path.abspath(__file__))
+FNALIBS_DIR = path.join(SCRIPT_DIR, "fnalibs")
+BUILD_DIR=path.join(SCRIPT_DIR, "build")
+CONFIGURE_OPTIONS = {
+    "SDL_SHARED": "ON",
+    "SDL_STATIC": "OFF",
+    "BUILD_SHARED_LIBS": "ON",
+    "FNALIBS_TARGET_DIRECTORY": os.getcwd()
+}
+def configure(cmake="cmake"):
+    args = [
+        cmake,
+        FNALIBS_DIR,
+        "-B",
+        BUILD_DIR
+    ]
+    for option in CONFIGURE_OPTIONS.keys():
+        args.append(f"-D{option}={CONFIGURE_OPTIONS[option]}")
+    return subprocess.call(args) == 0
+def build(cmake="cmake"):
+    args = [
+        cmake,
+        "--build",
+        BUILD_DIR,
+        "-j",
+        "8"
+    ]
+    return subprocess.call(args) == 0
+def install(cmake="cmake"):
+    args = [
+        "sudo",
+        cmake,
+        "--install",
+        BUILD_DIR
+    ]
+    return subprocess.call(args) == 0
+def main(options):
+    for lib in LIBS:
+        print(f"Cloning {lib.name}...")
+        if not lib.clone(FNALIBS_DIR):
+            print(f"Could not clone {lib.name}!")
+            exit(1)
+        else:
+            print(f"Successfully cloned {lib.name}!")
+    print(f"Configuring {FNALIBS_DIR}...")
+    if not configure(cmake=options.cmake):
+        print(f"Could not configure {FNALIBS_DIR}!")
+        exit(1)
+    else:
+        print(f"Successfully configured {FNALIBS_DIR}!")
+    print(f"Building {BUILD_DIR}")
+    if not build(cmake=options.cmake):
+        print(f"Could not build {BUILD_DIR}!")
+        exit(1)
+    else:
+        print(f"Successfully built {BUILD_DIR}!")
+    if os.name.lower() == "posix":
+        print(f"Installing FNA libs...")
+        if not install(cmake=options.cmake):
+            print(f"Could not install FNA libs!")
+            exit(1)
+        else:
+            print("Successfully installed FNA libs!")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cmake", help="Select the CMake command to use", type=str, default="cmake")
+    main(parser.parse_args())
